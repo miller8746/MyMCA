c7ce2c5f58a44b749d2e193511f831870c049cf8
Heather Miller <71408553+miller8746@users.noreply.github.com>
2022-02-16 12:04:33 -0600

    Fixing bundler route

--- src/Scripts/create_account.js
+++ src/Scripts/create_account.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue/dist/vue.esm-bundler.js'
+import { createApp } from './vue.esm-bundler.js'
 import AccountCreation from '../Components/AccountCreation.js'
 import { header } from "./header.js";
 
