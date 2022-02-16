1786176a7a46d003629c85908774d96383a76f29
Heather Miller <71408553+miller8746@users.noreply.github.com>
2022-02-16 12:04:50 -0600

    Fixing bundler route

--- src/Scripts/login.js
+++ src/Scripts/login.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue/dist/vue.esm-bundler.js'
+import { createApp } from './vue.esm-bundler.js'
 import Login from '../Components/Login.js'
 import { header } from "./header.js";
 
