490d9967acbabddbd98505143beda09a52268bca
Hannah H <61219036+hhunt-03@users.noreply.github.com>
2022-02-25 08:48:51 -0600

    adding enrollments api

--- MyMCA-dist/frontend/src/services/Service.js
+++ MyMCA-dist/frontend/src/services/Service.js
@@ -3,8 +3,11 @@ class ProfileService {
 	getCredentials(username, password) {
 		return http.get('/api/login/' + username + '&' + password);
 	}
-	getPrograms(userId) {
-		return http.get(`/api/users/${userId}/programs`);
+	getPrograms() {
+		return http.get(`/api/programs/`);
+	}
+	getEnrollments(){
+		return http.get('/api/enrollments/');
 	}
 }
 export default new ProfileService();
