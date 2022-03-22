c494ce9114805404e08257b640db39c88bdab6fe
Heather Miller <71408553+miller8746@users.noreply.github.com>
2022-03-22 16:06:03 -0500

    Updated query with Day table change

--- program-updates-3.sql
+++ program-updates-3.sql
@@ -1,4 +1,5 @@
 DROP TABLE Programs;
+DROP TABLE ProgramDays;
 
 CREATE TABLE Programs (
     ProgramId      INTEGER PRIMARY KEY
@@ -11,13 +12,26 @@ CREATE TABLE Programs (
     Location          STRING  NOT NULL,
     Cost              DOUBLE  NOT NULL,
     Capacity          INT     NOT NULL,
-    Days              STRING  NOT NULL,
     Instructor        INTEGER NOT NULL
                               REFERENCES Users
 );
 
-INSERT INTO Programs(Title, OfferingPeriod, OfferingPeriodEnd, Instructor, Description, Cost, Capacity, Location, Days)
-VALUES ('X-treme Cycling', '2022-07-06T07:00', '2022-07-06T08:00', 2, 'Hour long super intense workout!', 15.0, 10, 'Onalaska YMCA', 'Monday');
+CREATE TABLE ProgramDays (
+    ProgramDayId   INTEGER PRIMARY KEY
+			      UNIQUE
+			      NOT NULL,
+    ProgramId         INTEGER NOT NULL
+                              REFERENCES Programs,
+    Day               STRING  NOT NULL
+);
 
-INSERT INTO Programs(Title, OfferingPeriod, OfferingPeriodEnd, Instructor, Description, Cost, Capacity, Location, Days)
-VALUES ('Yoga Flow', '2022-07-07T07:00', '2022-07-07T08:00', 5, '45 min relaxing yoga session.', 12.0, 8, 'Dahl YMCA', 'Monday,Wednesday,Friday');
+INSERT INTO Programs(Title, OfferingPeriod, OfferingPeriodEnd, Instructor, Description, Cost, Capacity, Location)
+VALUES ('X-treme Cycling', '2022-07-06T07:00', '2022-07-06T08:00', 2, 'Hour long super intense workout!', 15.0, 10, 'Onalaska YMCA');
+
+INSERT INTO Programs(Title, OfferingPeriod, OfferingPeriodEnd, Instructor, Description, Cost, Capacity, Location)
+VALUES ('Yoga Flow', '2022-07-07T07:00', '2022-07-07T08:00', 5, '45 min relaxing yoga session.', 12.0, 8, 'Dahl YMCA');
+
+INSERT INTO ProgramDays(ProgramId, Day) VALUES (1, 'Monday');
+INSERT INTO ProgramDays(ProgramId, Day) VALUES (2, 'Monday');
+INSERT INTO ProgramDays(ProgramId, Day) VALUES (2, 'Wednesday');
+INSERT INTO ProgramDays(ProgramId, Day) VALUES (2, 'Friday');
