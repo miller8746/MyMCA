76d3537f864737b285d405857e87b5d8f821349a
hhunt-03 <daja.the.trader.mage@gmail.com>
2022-03-24 16:27:01 -0500

    added enrollment endpoints. no validation. refactored some stuff for better convention

--- MyMCA-dist/backend/index.js
+++ MyMCA-dist/backend/index.js
@@ -8,6 +8,10 @@ app.use(bodyParser.json());
 const port = 3000
 var router = express.Router();
 
+const allProgramAttributes = "ProgramId, Title, OfferingPeriod, OfferingPeriodEnd, Instructor, Description, Location, Cost, Capacity"
+const allUserAttributes = "UserId, Name,Member, Staff"
+const allCredentialAttributes = "Username, Password, UserId";
+
 // Log in with username and password
 app.get('/api/login/:user&:pass', (req, res) => {
   let username = req.params.user;
@@ -24,9 +28,15 @@ app.get('/api/login/:user&:pass', (req, res) => {
         });
 });
 
+/* PROGRAMS - all apis */
+
+/* GETS */
+
 // Get for Programs page
 app.get('/api/programs/', (req, res) => {
-  let sql = `SELECT ProgramId, Title, OfferingPeriod, OfferingPeriodEnd, Description, Cost, Capacity, Instructor FROM Programs ORDER BY OfferingPeriod ASC;`;
+  let sql = `SELECT ProgramId, Title, OfferingPeriod, OfferingPeriodEnd, Description, Cost, Capacity, Instructor 
+              FROM Programs 
+              ORDER BY OfferingPeriod ASC;`;
   db.all(sql, [], (err, rows) => {
     if (err) {
       console.log(err);
@@ -44,78 +54,11 @@ app.get('/api/programs/', (req, res) => {
   });
 });
 
-// Post for Create Programs page
-app.post(`/api/programs/`, (req, res) => {
-  let program = req.body;
-  console.log(program);
-
-  db.run(`INSERT INTO Programs(Title, OfferingPeriod, OfferingPeriodEnd, Instructor, Description, Location, Cost, Capacity) VALUES ('${program.programTitle}', '${program.programOfferingPeriod}', '${program.programOfferingPeriodEnd}', 2, '${program.programDescription}', '${program.programLocation}', ${program.programCost}, ${program.programCapacity});`)
-  .all(`SELECT * FROM Programs WHERE Title = '${program.programTitle}' AND OfferingPeriod = '${program.programOfferingPeriod}' AND OfferingPeriodEnd = '${program.programOfferingPeriodEnd}' AND Description = '${program.programDescription}' AND Location = '${program.programLocation}' AND Cost = ${program.programCost} AND Capacity = ${program.programCapacity}`, (err, rows) => {
-    // TODO: Is there a better way to select the program we just made, rather than selecting by all attributes?
-    // TODO: If not, add Instructor to the list of attributes we are selecting on if/when we implement instructors in programs
-    if (err) {
-      console.log(err);
-    }
-    db.serialize(() => {
-      // Queries will be forced to run sequentially
-      // Create entries in Days table
-      var i = 1;
-      for (i = 1; i < program.programDays.length; i++) {
-        db.run(`INSERT INTO ProgramDays(ProgramId, Day) VALUES (${rows[0].ProgramId}, '${program.programDays[i]}');`);
-      }
-      db.all(`SELECT ProgramDayId, Day FROM ProgramDays WHERE ProgramId = ${rows[0].ProgramId}`, (err, days) => {
-        if (err) {
-          console.log(err);
-        }
-        res.send(rows);
-      });
-    });
-  });;
-}); 
-
-// Post for Create Account page
-app.post('/api/create-account/', (req,res) => {
-  let credentials = req.body;
-  console.log(credentials);
-
-  let sql = `SELECT * FROM Credentials WHERE Username = '${credentials.username}';`
-
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      console.log(err);
-    }
-    
-    // Check for existing username
-    if (rows[0] != null) {
-      res.send({successful: false, userId: 0});
-    } else {
-      // Put values into Users and Credentials tables (unique UserId != 0)
-      db.run(`INSERT INTO Users(Name, Member, Staff) VALUES ('${credentials.name}', ${credentials.isMember}, ${credentials.isStaff})`)
-        .all(`SELECT * FROM Users WHERE Name = '${credentials.name}' AND Member = ${credentials.isMember} AND Staff = ${credentials.isStaff}`, (err, rows) => {
-          // TODO: Better way to select row after creation?
-          if (err) {
-            throw err;
-          }
-          if (rows[0] == null) {
-            console.log('Credential was accessed before created.');
-          } else {
-            var id = rows[0].UserId;
-	    // Create corresponding entry in Credentials table
-            db.run(`INSERT INTO Credentials(Username, Password, UserId) VALUES ('${credentials.username}', '${credentials.password}', ${id})`).all(`SELECT * FROM Credentials WHERE UserId = ${id}`, (err, rows) => {
-              if (err) {
-                console.log(err);
-	      }
-              res.send({successful: true, userId: id});
-            });
-          }
-      });
-    }
-  });
-})
-
 // Get enrollments for Programs page
 app.get('/api/enrollments/', (req, res) => {
-  let sql = `SELECT p.ProgramId, Count(e.programId) AS NumOfEnrollments FROM Programs p JOIN Enrollments e ON e.ProgramId = p.ProgramId GROUP BY p.ProgramId`;
+  let sql = `SELECT p.ProgramId, Count(e.programId) AS NumOfEnrollments 
+                FROM Programs p JOIN Enrollments e ON e.ProgramId = p.ProgramId 
+                GROUP BY p.ProgramId`;
   let enrollments = [];
 
   db.all(sql, [], (err, rows) => {
@@ -133,10 +76,70 @@ app.get('/api/enrollments/', (req, res) => {
   });
 });
 
+/* POSTS */
+
+app.post('/api/users/:userId/enrollments/:programId/', (req, res) => {
+  const userId = req.params.userId;
+  const programId = req.params.programId; 
+
+  db.run(`INSERT INTO Enrollments(UserId, ProgramId) 
+          VALUES(${userId}, ${programId});`)
+    .all(`SELECT EnrollmentId 
+            FROM Enrollments 
+            WHERE UserId = ${userId} AND ProgramId = ${programId};`, (err, rows) => {
+              if( err ){
+                console.log(error);
+              } else {
+                res.send(rows);
+              }
+            });
+}); 
+
+// Post for Create Programs page
+app.post(`/api/programs/`, (req, res) => {
+  let program = req.body;
+  console.log(program);
+
+  db.run(`INSERT INTO Programs(Title, OfferingPeriod, OfferingPeriodEnd, Instructor, Description, Location, Cost, Capacity) VALUES ('${program.programTitle}', '${program.programOfferingPeriod}', '${program.programOfferingPeriodEnd}', 2, '${program.programDescription}', '${program.programLocation}', ${program.programCost}, ${program.programCapacity});`)
+  .all(`SELECT ${allProgramAttributes} 
+          FROM Programs 
+          WHERE Title = '${program.programTitle}' AND 
+                OfferingPeriod = '${program.programOfferingPeriod}' AND 
+                OfferingPeriodEnd = '${program.programOfferingPeriodEnd}' AND 
+                Description = '${program.programDescription}' AND 
+                Location = '${program.programLocation}' AND 
+                Cost = ${program.programCost} AND 
+                Capacity = ${program.programCapacity}`, (err, rows) => {
+    if (err) {
+      console.log(err);
+    }
+    db.serialize(() => {
+      var i = 1;
+      for (i = 1; i < program.programDays.length; i++) {
+        db.run(`INSERT INTO ProgramDays(ProgramId, Day) VALUES (${rows[0].ProgramId}, '${program.programDays[i]}');`);
+      }
+      db.all(`SELECT ProgramDayId, Day FROM ProgramDays WHERE ProgramId = ${rows[0].ProgramId}`, (err, days) => {
+        if (err) {
+          console.log(err);
+        }
+        res.send(rows);
+      });
+    });
+  });;
+}); 
+
+
+
+/* USERS - all apis */
+
+/* GETS */
+
+/* POSTS */
+
 // Post for updating credentials on Profile page
-app.post('/api/save-account-info/', (req, res) => {
+app.post('/api/account-info/', (req, res) => {
   db.run(`UPDATE Users SET Member = '${req.body.isMember}', Staff = '${req.body.isStaff}' WHERE UserId = ${req.body.userId};`)
-  .all(`SELECT * FROM Users WHERE UserId = ${req.body.userId};`, (err, rows) => {
+  .all(`SELECT ${allUserAttributes} FROM Users WHERE UserId = ${req.body.userId};`, (err, rows) => {
     if (err) {
       console.log(err);
     } else {
@@ -145,8 +148,50 @@ app.post('/api/save-account-info/', (req, res) => {
   });
 });
 
+// Post for Create Account page
+app.post('/api/account/', (req,res) => {
+  let credentials = req.body;
+  console.log(credentials);
+
+  let sql = `SELECT ${allCredentialAttributes} FROM Credentials WHERE Username = '${credentials.username}';`
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      console.log(err);
+    }
+    
+    // Check for existing username
+    if (rows[0] != null) {
+      res.send({successful: false, userId: 0});
+    } else {
+      // Put values into Users and Credentials tables (unique UserId != 0)
+      db.run(`INSERT INTO Users(Name, Member, Staff) VALUES ('${credentials.name}', ${credentials.isMember}, ${credentials.isStaff})`)
+        .all(`SELECT ${allUserAttributes} FROM Users WHERE Name = '${credentials.name}' AND Member = ${credentials.isMember} AND Staff = ${credentials.isStaff}`, (err, rows) => {
+          // TODO: Better way to select row after creation?
+          if (err) {
+            throw err;
+          }
+          if (rows[0] == null) {
+            console.log('Credential was accessed before created.');
+          } else {
+            var id = rows[0].UserId;
+	    // Create corresponding entry in Credentials table
+            db.run(`INSERT INTO Credentials(Username, Password, UserId) VALUES ('${credentials.username}', '${credentials.password}', ${id})`).all(`SELECT ${allCredentialAttributes} FROM Credentials WHERE UserId = ${id}`, (err, rows) => {
+              if (err) {
+                console.log(err);
+	      }
+              res.send({successful: true, userId: id});
+            });
+          }
+      });
+    }
+  });
+})
+
+// OTHER
+
 app.listen(port, () => {
-  console.log(`MyMCA backend listening on port ${port}`)
+  console.log(`MyMCA backend listening on port ${port}`) // REMOVE FOR PROD
 })
 
 // DB Connection
@@ -159,5 +204,5 @@ let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     return console.error(err.message);
   }
-  console.log('Connected to the MyMCA database.');
+  console.log('Connected to the MyMCA database.'); // REMOVE FOR PROD
 });
